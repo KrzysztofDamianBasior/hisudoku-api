d7709c62cabc57ec70ad05d1ab7c119196ade68e
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-02-20 23:26:40 +0100

    feat: add an auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { validate } from './env.validation';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { UsersModule } from './users/users.module';
       }),
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
