13f7f4ed52d0c2c6e1d5ffe28aa547dd7de1a655
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2023-02-20 23:08:46 +0100

    feat: feat: add an .env validation and a global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+// constructor(private configService: ConfigService<EnvironmentVariables>
+// const port = this.configService.get('PORT', { infer: true });
+// const dbUser = this.configService.get<string>('DATABASE_USER');
