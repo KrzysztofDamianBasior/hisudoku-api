b2bbfe072bd2856cf41eb969ec98df54f1e48461
Krzysztof Damian Basior <KrzysztofDamianBasior@gmail.com>
2025-04-02 23:18:39 +0200

    feat: add MessageResponseModel type referring to gql schema

---
+++ src/main/java/org/hisudoku/hisudokuapi/users/models/MessageResponseModel.java
@@ -0,0 +1,11 @@
+package org.hisudoku.hisudokuapi.users.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponseModel {
+    private String message;
+}
+
